3d3ecdba2c24858dff5a36e5b610986a232182c5
malloryarcher46@gmail.com <malloryarcher46@gmail.com>
2022-07-23 11:23:15 -0400

    fetch

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-const baseUrl = "https://ghibliapi.herokuapp.com"
+const baseUrl = "https://ghibliapi.herokuapp.com/films"
 
 const searchDiv = document.getElementById("searchContainer")
 const containerDiv = document.getElementById("container")
 
-fetch("https://ghibliapi.herokuapp.com")
+
+fetch(baseUrl)
 .then(res => res.json())
-.then(data => {
-    console.log(data)
+.then(films => {
+    console.log(films)
 })
-.catch(error => console.log(error))
+.catch(error => (console.log(`Error: ${error}`)))
+
+films.results.forEach(films => {
+        makeFilmDiv(image, movie, divForInfo)
+})
+
+getFilmResults = () => {
+    const searchInput = getElementById("search")
+
+    fetch(`https://ghibliapi.herokuapp.com/films${searchInput}`)
+    .then(res => res.json())
+    .then(films => {
+        console.log(films)
+    })
+    .catch(error => (console.log(`Error: ${error}`)))
+
+    films.results.forEach(films => {
+        makeFilmDiv(image, movie, divForInfo)
+    })
+
+    searchInput.value = ""
+}
+
+const makeFilmDiv = (image, movie, divForInfo) => {
+    const filmDiv = document.createElement('div')
+
+    const filmPic = document.createElement('img')
+    const filmName = document.createElement("h3")
+
+    filmPic.src = image
+    filmName = movie
+    
+    filmDiv.appendChild(filmPic)
+    filmDiv.appendChild(filmName)
+
+    divForInfo.appendChild(filmDiv)
+}
